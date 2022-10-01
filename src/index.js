f03d7f4330db93ebde7c5681d7b992d30df21669
ThiagoFTelles <atendimento.telles@gmail.com>
2022-10-01 15:04:46 -0300

    feat: add Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+function contador(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = configureStore({
+  reducer: contador,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
